706472fdff8c9714812c91891f394ffcf4bb9dd2
Michael Aldridge <aldridge.mac@gmail.com>
2015-12-05 19:47:43 -0600

    split on semicolon

--- mshell.c
+++ mshell.c
@@ -1,9 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
+
+typedef struct Commands {
+  char* cmd;
+  char* args;
+  bool rdrstdout;
+  bool rdrstdin;
+  bool file2stdin;
+  bool stdout2file;
+  char* infname;
+  char* outfname;
+} Command;
 
 void shell_loop();
 char* shell_getline();
+Command* shell_splitLine(char*);
+char** split_on_token(char*,char*);
 
 int main(int argc, char** argv) {
   printf("Welcome to mShell\n");
@@ -20,6 +34,9 @@ void shell_loop() {
   //get the data from the line
   char* line = shell_getline();
   printf(line);
+
+  //pase the line
+  shell_splitLine(line);
 }
 
 char* shell_getline() {
@@ -30,4 +47,48 @@ char* shell_getline() {
   ssize_t bufsize = 0;
   getline(&line, &bufsize, stdin);
   return line;
+}
+
+Command* shell_splitLine(char* line) {
+  Command* cmds = NULL;
+  char** jobs = NULL;
+  printf("Got line: %s", line);
+  jobs = split_on_token(line, ";");
+
+  int i;
+  for(i=0; jobs[i] != NULL; i++) {
+    //first we work on individual jobs
+    printf("job: %s\n", jobs[i]);
+  }
+  return cmds;
+}
+
+char** split_on_token(char* line, char* stoken) {
+  int bufsize = 64, position = 0;
+  char **tokens = malloc(bufsize * sizeof(char*));
+  char *token;
+
+  if (!tokens) {
+    fprintf(stderr, "mShell: Couldn't allocate token buffer\n");
+    exit(EXIT_FAILURE);
+  }
+
+  token = strtok(line, stoken);
+  while (token != NULL) {
+    tokens[position] = token;
+    position++;
+
+    if (position >= bufsize) {
+      bufsize += 64;
+      tokens = realloc(tokens, bufsize * sizeof(char*));
+      if (!tokens) {
+	fprintf(stderr, "mShell: Couldn't reallocate token buffer\n");
+	exit(EXIT_FAILURE);
+      }
+    }
+
+    token = strtok(NULL, stoken);
+  }
+  tokens[position] = NULL;
+  return tokens;
 }
